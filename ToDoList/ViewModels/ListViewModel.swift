f89fcead28abf1114a3e34584e143a642e096b5c
Vahtee Boo <vahtee.boo@gmail.com>
2022-02-08 22:39:52 +0200

    Some optimizations

---
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -0,0 +1,35 @@
+//
+//  ListViewModel.swift
+//  ToDoList
+//
+//  Created by Vahtee Boo on 08.02.2022.
+//
+
+import Foundation
+
+class ListViewModel: ObservableObject {
+    
+    @Published var items: [ItemModel] = []
+    
+    init() {
+        getItems()
+    }
+    
+    func getItems() {
+        let newItems = [
+            ItemModel(title: "This is the first lohness creature", isCompleted: false),
+            ItemModel(title: "This is the second!", isCompleted: true),
+            ItemModel(title: "Third!", isCompleted: false)
+        ]
+        items.append(contentsOf: newItems)
+    }
+    
+    func deleteItem(indexSet: IndexSet) {
+        items.remove(atOffsets: indexSet)
+    }
+    
+    func moveItem(from: IndexSet, to: Int) {
+        items.move(fromOffsets: from, toOffset: to)
+    }
+    
+}
--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -9,32 +9,21 @@ import SwiftUI
 
 struct ListView: View {
     
-    @State var items: [ItemModel] = [
-        ItemModel(title: "This is the first lohness creature", isCompleted: false),
-        ItemModel(title: "This is the second!", isCompleted: true),
-        ItemModel(title: "Third!", isCompleted: false)
-    ]
+    @EnvironmentObject var listViewModel: ListViewModel
+
     var body: some View {
         List {
-            ForEach(items) {
+            ForEach(listViewModel.items) {
                 item in ListRowView(item: item)
             }
-            .onDelete(perform: deleteItem)
-            .onMove(perform: moveItem)
+            .onDelete(perform: listViewModel.deleteItem)
+            .onMove(perform: listViewModel.moveItem)
         }
         .listStyle(PlainListStyle())
         .navigationTitle("TodoList 📝")
         .navigationBarItems(
             leading: EditButton(),
             trailing: NavigationLink("Add", destination: AddView()))
-    }
-    
-    func deleteItem(indexSet: IndexSet) {
-        items.remove(atOffsets: indexSet)
-    }
-    
-    func moveItem(from: IndexSet, to: Int) {
-        items.move(fromOffsets: from, toOffset: to)
     }
     
 }
@@ -44,6 +33,7 @@ struct ListView_Previews: PreviewProvider {
         NavigationView {
             ListView()
         }
+        .environmentObject(ListViewModel())
     }
 }
 
