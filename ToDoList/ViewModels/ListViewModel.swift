26dfa7ec5b7d516421cd2db93b4b80fdce0289ea
Vahtee Boo <vahtee.boo@gmail.com>
2022-02-09 18:27:01 +0200

    ViewModel ready

--- ToDoList/ViewModels/ListViewModel.swift
+++ ToDoList/ViewModels/ListViewModel.swift
@@ -32,4 +32,15 @@ class ListViewModel: ObservableObject {
         items.move(fromOffsets: from, toOffset: to)
     }
     
+    func addItem(title: String) {
+        let newItem = ItemModel(title: title, isCompleted: false)
+        items.append(newItem)
+    }
+    
+    func updateItem(item: ItemModel) {
+        
+        if let index = items.firstIndex(where: { $0.id == item.id }) {
+            items[index] = item.updateCompletion()
+        }
+    }
 }
--- ToDoList/Views/AddView.swift
+++ ToDoList/Views/AddView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct AddView: View {
     
+    @Environment(\.presentationMode) var presentationMode
+    @EnvironmentObject var listViewModel: ListViewModel
     @State var textFieldText = ""
+    
+    @State var alertTitle: String = ""
+    @State var showAlert: Bool = false
     
     var body: some View {
         ScrollView {
@@ -21,7 +26,7 @@ struct AddView: View {
                     .cornerRadius(10)
                 
                 Button {
-                    
+                    saveButtonPressed()
                 } label: {
                     Text("Save".uppercased())
                         .foregroundColor(.white)
@@ -31,11 +36,31 @@ struct AddView: View {
                         .background(Color.accentColor)
                         .cornerRadius(10)
                 }
-
             }
             .padding(14)
         }
         .navigationTitle("Add an Item 🖋")
+        .alert(isPresented: $showAlert, content: getAlert) 
+    }
+    
+    func saveButtonPressed() {
+        if textIsValid() {
+            listViewModel.addItem(title: textFieldText)
+            presentationMode.wrappedValue.dismiss()
+        }
+    }
+    
+    func textIsValid() -> Bool {
+        if textFieldText.count < 5 {
+            alertTitle = "Your new todo item must be at least 5 characters long!!! 😨😱"
+            showAlert.toggle()
+            return false
+        }
+        return true
+    }
+    
+    func getAlert() -> Alert {
+        return Alert(title: Text(alertTitle))
     }
 }
 
