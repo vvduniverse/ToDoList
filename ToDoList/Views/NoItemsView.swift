3367a194a5f6a40053c0db9f265061e6ff437712
Vahtee Boo <vahtee.boo@gmail.com>
2022-02-10 16:36:09 +0200

    Some animation added

---
+++ ToDoList/Views/NoItemsView.swift
@@ -0,0 +1,69 @@
+//
+//  NoItemsView.swift
+//  ToDoList
+//
+//  Created by Vahtee Boo on 10.02.2022.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("The wallet is empty!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("If you want to earn some money add a bunch of items to your todo list and do them!")
+                    .padding(.bottom, 20)
+                NavigationLink(destination: AddView()) {
+                    Text("Add Something 🤯")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : Color.accentColor)
+                        .cornerRadius(10)
+                }
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0.0,
+                    y: animate ? 50 : 30)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+        }
+        }
+    }
+}
+
+struct NoItemsView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            NoItemsView()
+                .navigationTitle("Title")
+        }
+    }
+}
